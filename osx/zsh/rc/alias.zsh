c0e1fedbccb46db09bf8cb088c401d24493b05ae
Calum MacRae <cmacrae@onyx.local>
2014-06-17 18:24:57 +0100

    Syncing over OS X dotfiles from master branch. Will be using master branch for desktop OS X

--- osx/zsh/rc/alias.zsh
+++ osx/zsh/rc/alias.zsh
@@ -1,6 +1,5 @@
+alias ls="ls -G"
 alias rm="rm -i"
 alias cp="cp -i"
-alias ls="ls -G"
-alias reboot="shutdown -r now"
-alias getbat='pmset -g batt | egrep "([0-9]+\%).*" -o | cut -f1 -d";"'
-alias map-home="source ~/.private/map-home"
+alias ytdl="youtube-dl -f mp4 --max-quality mp4 -o '%(title)s.%(ext)s'"
+alias gows="cd $GOWORKSPACE"
