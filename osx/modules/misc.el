8bacc38e54c42c4b38d91f5d5161714452f7983b
cmacrae <calum0macrae@gmail.com>
2014-09-03 23:04:43 +0100

    Including emacs modules

---
+++ osx/modules/misc.el
@@ -0,0 +1,10 @@
+;; -------------------------------------
+;; MISC
+;; -------------------------------------
+(require 'ido)
+(ido-mode t)
+
+;; deactivate
+(menu-bar-mode 0)
+(setq make-backup-files nil)
+(setq auto-save-default nil)
