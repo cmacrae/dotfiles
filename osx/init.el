e48869afcb63a8aaad2d1fac2fb2a8373d2044ff
cmacrae <calum0macrae@gmail.com>
2014-09-03 22:47:38 +0100

    General update & move to emacs

---
+++ osx/init.el
@@ -0,0 +1,38 @@
+;; -------------------------------------
+;; PACKAGES
+;; -------------------------------------
+
+(require 'cl)
+(require 'package)
+
+(add-to-list 'package-archives
+
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(go-mode markdown-mode)
+  "Packages to ensure are installed upon launch.")
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; load vendor & custom files
+(defvar emacs-dir (file-name-directory load-file-name)
+  "top level emacs directory")
+(defvar vendor-dir (concat emacs-dir "vendor/")
+  "Packages not yet available in ELPA")
+(defvar module-dir (concat emacs-dir "modules/")
+  "Configuration componants")
+
+(add-to-list 'load-path vendor-dir)
+(add-to-list 'load-path module-dir)
+
+;; require packages in modules/
+(mapc 'load (directory-files module-dir nil "^[^#].*el$"))
+(server-start)
