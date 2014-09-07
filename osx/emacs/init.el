5089b11b8d00f157db1ee1b6258df62b77c1e7bb
cmacrae <calum0macrae@gmail.com>
2014-09-07 22:06:35 +0100

    Removing stuff added by custom from init.el

--- osx/emacs/init.el
+++ osx/emacs/init.el
@@ -36,15 +36,3 @@
 (mapc 'load (directory-files module-dir nil "^[^#].*el$"))
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (server-start)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(znc-servers (quote (("irc.cmacr.ae" 1025 t ((freenode "cmacrae" "DiDac7")))))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
