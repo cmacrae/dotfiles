4859843762475105f0df9740d8b00bfa29ceb9a3
cmacrae <calum0macrae@gmail.com>
2014-11-17 20:37:59 +0000

    Adding MEPLA & using moe-theme

--- osx/emacs/init.el
+++ osx/emacs/init.el
@@ -6,9 +6,8 @@
 (require 'package)
 
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
-(add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
-
+(add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -16,7 +15,7 @@
 
 (defvar my-packages '(erc-hl-nicks flycheck magit yasnippet
 		      go-mode markdown-mode json-mode yaml-mode
-		      projectile puppet-mode elfeed)
+		      projectile puppet-mode elfeed moe-theme)
   "Packages to ensure are installed upon launch.")
 (dolist (p my-packages)
   (when (not (package-installed-p p))
--- osx/emacs/modules/appearance.el
+++ osx/emacs/modules/appearance.el
@@ -2,6 +2,15 @@
 ;; APPEARANCE
 ;; -------------------------------------
 
-;; theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'junio t)
+;; hide stuff
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(menu-bar-mode 0)
+
+;; moe-theme
+(require 'moe-theme)
+(moe-theme-set-color 'green)
+(moe-light)
+;; resize titles
+(setq moe-theme-resize-markdown-title '(2.0 1.7 1.5 1.3 1.0 1.0))
+(setq moe-theme-resize-org-title '(2.0 1.8 1.6 1.4 1.2 1.0 1.0 1.0 1.0))
