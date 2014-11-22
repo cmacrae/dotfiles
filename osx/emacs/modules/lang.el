2ab211712772d1738b292bbe9f00bf102207a5ba
cmacrae <calum0macrae@gmail.com>
2014-11-22 14:09:40 +0000

    Adding custom keybindings/vars for haskell development

--- osx/emacs/modules/lang.el
+++ osx/emacs/modules/lang.el
@@ -6,6 +6,25 @@
   (setenv "PATH" (concat cabal-bin-path ":" (getenv "PATH")))
   (add-to-list 'exec-path cabal-bin-path))
 (custom-set-variables '(haskell-tags-on-save t))
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-type 'cabal-repl)
+  '(haskell-process-log t)
+  '(haskell-tags-on-save t))
+(eval-after-load 'haskell-mode '(progn
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
+  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
+(eval-after-load 'haskell-cabal '(progn
+  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
 
 ;; go-mode
 (require 'go-mode)
