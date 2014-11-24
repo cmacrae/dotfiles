0a66a217b41762560193d91408d4c95bb4a8d23e
cmacrae <calum0macrae@gmail.com>
2014-11-24 22:40:16 +0000

    Setting gtd.org as agenda & adding capture templates

--- osx/emacs/modules/org-mode.el
+++ osx/emacs/modules/org-mode.el
@@ -5,3 +5,10 @@
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+(setq org-return-follows-link t)
+(setq org-agenda-files "~/org/gtd.org")
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/org/gtd.org" "Tasks")
+	 "* TODO %^{Brief Description} %^g\n%?\tAdded: %U")
+	("r" "ToRead" entry (file+headline "~/org/gtd.org" "Projects")
+	 "* TOREAD %^{Title} %^g\n%?\tLink: %c")))
