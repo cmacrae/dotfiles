ac89ba6ebba201bd7c892351ca9922fe9543a061
cmacrae <calum0macrae@gmail.com>
2014-12-27 16:42:03 +0000

    Cleaner element hiding

--- emacs/modules/appearance.el
+++ emacs/modules/appearance.el
@@ -3,9 +3,12 @@
 ;; -------------------------------------
 
 ;; hide stuff
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
-(menu-bar-mode 0)
+
+(when window-system
+  (tooltip-mode -1)
+  (tool-bar-mode -1)
+  (menu-bar-mode -1)
+  (scroll-bar-mode -1))
 
 ;; moe-theme
 (require 'moe-theme)
