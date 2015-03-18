78461f62a24255912c99e7dde6a8749943b037f0
cmacrae <calum0macrae@gmail.com>
2015-03-18 21:09:52 +0000

    Cleaning up the modeline, customizing clock format

--- emacs/modules/appearance.el
+++ emacs/modules/appearance.el
@@ -11,6 +11,7 @@
 
 ;; clock
 (display-time-mode 1)
+(setq display-time-format "%H:%M %a %d %b")
 
 ;; highlight current line
 (global-hl-line-mode t)
@@ -19,3 +20,28 @@
 (require 'moe-theme)
 (require 'moe-theme-switcher)
 (moe-theme-set-color 'green)
+
+;; mode line
+(setq-default mode-line-format
+              (quote
+               ("   "
+                ;; mode
+                (:propertize global-mode-string face 'mode-line-mode-string)
+
+                ;; file path
+                (:propertize (:eval (if (> (length default-directory) 17)
+                                        (concat "..." (substring default-directory -20))
+                                      default-directory))
+                             face 'mode-line-folder-face)
+
+                ;; file/buffer name
+                (:propertize mode-line-buffer-identification face 'mode-line-buffer-name)
+                (:propertize mode-line-modified face 'mode-line-modified-face)
+		"  "
+		(vc-mode vc-mode)
+		"  "
+                ;; 'mode-name'
+                (:propertize "%m" face 'mode-line-mode-name)
+                " :: "
+                ;; line number
+                "Line %l, %p")))
