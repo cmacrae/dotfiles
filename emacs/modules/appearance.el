80cc3cd19cef767c917898556d65d61fffe9c78c
cmacrae <calum0macrae@gmail.com>
2015-03-22 13:12:40 +0000

    Adding custom function for file/buffer name

--- emacs/modules/appearance.el
+++ emacs/modules/appearance.el
@@ -24,20 +24,28 @@
 (moe-theme-set-color 'green)
 
 ;; mode line
+(defun shorten-directory (dir max-length)
+  "Show up to `max-length' characters of a directory name `dir'."
+  (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
+        (output ""))
+    (when (and path (equal "" (car path)))
+      (setq path (cdr path)))
+    (while (and path (< (length output) (- max-length 4)))
+      (setq output (concat (car path) "/" output))
+      (setq path (cdr path)))
+    (when path
+      (setq output (concat ".../" output)))
+    output))
+
 (setq-default mode-line-format
               (quote
                ("   "
                 ;; mode
                 (:propertize global-mode-string 'face 'mode-line-mode-string)
-
-                ;; file path
-                (:propertize (:eval (if (> (length default-directory) 17)
-                                        (concat "..." (substring default-directory -20))
-                                      default-directory))
-                             'face 'mode-line-folder-face)
-
-                ;; file/buffer name
-                (:propertize mode-line-buffer-identificatino 'face 'mode-line-buffer-name)
+		(:propertize (:eval (shorten-directory default-directory 10))
+			     'face 'mode-line-folder-face)
+		(:propertize "%b"
+			     'face 'mode-line-filename-face)
 		" "
                 (:propertize mode-line-modified 'face 'mode-line-modified-face)
 		"  "
