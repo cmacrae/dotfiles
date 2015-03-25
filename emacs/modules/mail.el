ad8ab5f8a8b75ea95e20aeab4cee50cec0cc81d1
cmacrae <calum0macrae@gmail.com>
2015-03-25 22:20:55 +0000

    Adding mu4e 'View in browser' action

--- emacs/modules/mail.el
+++ emacs/modules/mail.el
@@ -46,6 +46,23 @@
 (setq gnus-dired-mail-mode 'mu4e-user-agent)
 (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
 
+;; mu4e - view in browser action
+(defun mu4e-msgv-action-view-in-browser (msg)
+  "View the body of the message in a web browser."
+  (interactive)
+  (let ((html (mu4e-msg-field (mu4e-message-at-point t) :body-html))
+	(tmpfile (format "%s/%d.html" temporary-file-directory (random))))
+    (unless html (error "No html part for this message"))
+    (with-temp-file tmpfile
+      (insert
+       "<html>"
+       "<head><meta http-equiv=\"content-type\""
+       "content=\"text/html;charset=UTF-8\">"
+       html))
+    (browse-url (concat "file://" tmpfile))))
+(add-to-list 'mu4e-view-actions
+	     '("View in browser" . mu4e-msgv-action-view-in-browser) t)
+
 ;; smtpmail
 (require 'smtpmail)
 (require 'starttls)
