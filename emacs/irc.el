5c4d8d1c3266d7b792aa413582e6dbed0ff650a9
cmacrae <calum0macrae@gmail.com>
2015-05-09 13:24:45 +0100

    Removing irc.el (in the middle of transitioning to rcirc)

--- emacs/irc.el
+++
@@ -1,6 +0,0 @@
-;; -------------------------------------
-;; ZNC + ERC
-;; -------------------------------------
-(setq znc-servers (quote (("irc.cmacr.ae" 1025 t ((freenode "cmacrae" "XXXX"))))))
-(require 'erc-match)
-(setq erc-keywords '("resolve" "cmacrae"))
