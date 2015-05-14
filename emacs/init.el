81e195a0fd5252b971390c199b9973d5e604fc94
cmacrae <calum0macrae@gmail.com>
2015-05-14 20:05:52 +0100

    Moving annoying custom vars to other file...

--- emacs/init.el
+++ emacs/init.el
@@ -40,18 +40,6 @@
 (mapc 'load (directory-files module-dir nil "^[^#].*el$"))
 (mapc 'load (directory-files vendor-dir nil "^[^#].*el$"))
 (add-hook 'after-init-hook #'global-flycheck-mode)
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 (server-start)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (popwin yaml-mode shm sauron rainbow-delimiters puppet-mode org-page org-bullets openwith neotree multiple-cursors moe-theme markdown-mode magit json-mode jinja2-mode helm-projectile haskell-mode go-mode flycheck exec-path-from-shell erc-hl-nicks elfeed diff-hl company-ghc ansible-doc ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
